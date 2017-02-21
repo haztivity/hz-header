0d14d0256a7ca941f4db8d95f4ec88f1469d29c4
Javier Pérez Ruiz <javier.perez@davinchi.es>
2017-02-21 18:42:09 +0100

    Compilado a UMD

--- src/HzHeaderComponent.js
+++ src/HzHeaderComponent.js
@@ -1,37 +1,14 @@
 var __extends = (this && this.__extends) || function (d, b) {
-        for (var p in b) if (b.hasOwnProperty(p)) {
-            d[p] = b[p];
-        }
-        function __() {
-            this.constructor = d;
-        }
-
-        d.prototype = b === null
-            ? Object.create(b)
-            : (__.prototype = b.prototype, new __());
-    };
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3
-            ? target
-            : desc === null
-                                          ? desc = Object.getOwnPropertyDescriptor(target, key)
-                                          : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") {
-            r = Reflect.decorate(decorators,
-                                 target,
-                                 key,
-                                 desc);
-        } else {
-            for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) {
-                r = (c < 3
-                        ? d(r)
-                        : c > 3
-                         ? d(target, key, r)
-                         : d(target, key)) || r;
-            }
-        }
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    };
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
@@ -81,9 +58,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
          * @param {boolean}             [asHtml=false]      Indica si establecer el título como texto o html. Se utiliza $.text o $.html
          */
         HzHeaderComponent.prototype.setTitle = function (title, asHtml) {
-            if (asHtml === void 0) {
-                asHtml = false;
-            }
+            if (asHtml === void 0) { asHtml = false; }
             this.clear();
             if (title instanceof this._$) {
                 this._$title.append(title);
--- test/course/1221/pages/6611/6611.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * Copyright Davinchi. All Rights Reserved.
- */
-import {Page} from "@haztivity/core";
-declare let page: Page;
-export {page as page6611};
